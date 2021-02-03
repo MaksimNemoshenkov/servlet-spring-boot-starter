031923347f50041e6a8afa29ee6410b14b9a49f6
MaksimNemoshenkov <shalun084@gmail.com>
2021-02-03 21:59:49 +0200

    starter

---
+++ src/main/java/com/configuration/ServletAutoConfiguration.java
@@ -0,0 +1,17 @@
+package com.configuration;
+
+import com.servlet.Servlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ServletAutoConfiguration implements WebMvcConfigurer {
+    @Bean
+    public ServletRegistrationBean getMyServletBean() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new Servlet(), "/just");
+        bean.setLoadOnStartup(1);
+        return bean;
+    }
+}
