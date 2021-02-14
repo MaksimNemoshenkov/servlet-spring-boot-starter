0c79035ab9fe105a89bc203c1f52c03926cd7db1
MaksimNemoshenkov <shalun084@gmail.com>
2021-02-14 20:47:13 +0200

    add custom date Header

--- src/main/java/com/servlet/Servlet.java
+++ src/main/java/com/servlet/Servlet.java
@@ -5,9 +5,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.Instant;
 
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.addDateHeader("myDate", Instant.now().toEpochMilli());
     }
 }
